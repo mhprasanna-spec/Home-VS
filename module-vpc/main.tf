f1726283b8f6811882ad57ae8caa0860d038497d
mhprasanna-spec <mhprasanna@gmail.com>
2026-03-10 17:47:12 +0530

    corr2

--- module-vpc/main.tf
+++ module-vpc/main.tf
@@ -1,15 +1,13 @@
 provider "aws" {
-    region = var.aws_region
-  
+  region = var.aws_region
 }
 
-module vpc_network {
+module "vpc_network" {
 
-source = "./modules/vpc"
-aws_region = var.aws_region
-vpc_cidr = var.vpc_cidr
-public_subnet_cidrs = var.public_subnet_cidrs
-private_subnet_cidrs = var.private_subnet_cidrs
-availability_zones = var.availability_zones
+  source = "./modules/vpc"
 
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
 }
